eb6c35a1251ef2c14c1f0092ba3aed2bf3a63353
Xhunik Miguel <xhuniktzi@gmail.com>
2020-08-20 21:20:57 -0600

    feature: finalizadoas estructura de pantallas iniciales

---
+++ src/views/start_page.py
@@ -0,0 +1,9 @@
+# Vista pantalla de Inicio
+
+def render():
+    print('Lenguajes formales y de programacion')
+    print('Nombre: Xhunik Miguel')
+    print('Seccion: B')
+    print('Carnet: 201900462')
+    print('\nPresione enter para continuar')
+    pass
