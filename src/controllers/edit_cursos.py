1904d6185d741beee3f0338844f51a3ca5d187d5
Xhunik Miguel <xhuniktzi@gmail.com>
2020-09-02 13:51:41 -0600

    fix: validacion editar cursos

--- src/controllers/edit_cursos.py
+++ src/controllers/edit_cursos.py
@@ -11,38 +11,48 @@ def exec(database):
     print('*** Editar curso ***')
     print('Ingresa el codigo del curso a editar: ', end='')
     code = int(input())
-    curso = read(database, codigo=code)[0]
+    try:
+        curso = read(database, codigo=code)[0]
+        pass
+    except IndexError:
+        print('No existe el curso')
+        pass
+    else:
+        print('Ingresa el codigo, actual: (' + str(curso.codigo) + ') ', end='')
+        curso_codigo = int(input() or curso.codigo)
+        new_curso.codigo = curso_codigo
 
-    print('Ingresa el codigo, actual: (' + str(curso.codigo) + ') ', end='')
-    curso_codigo = int(input() or curso.codigo)
-    new_curso.codigo = curso_codigo
+        print('Ingresa el nombre, actual: (' + curso.nombre + ') ', end='')
+        curso_nombre = input() or curso.nombre
+        new_curso.nombre = curso_nombre
 
-    print('Ingresa el nombre, actual: (' + curso.nombre + ') ', end='')
-    curso_nombre = input() or curso.nombre
-    new_curso.nombre = curso_nombre
+        print('Ingresa los pre-requisitos, separados con ";", actual: (' +
+              str(curso.codigo_requisitos) + ') ', end='')
+        curso_prerequisitos = parse_requisitos(
+            input()) or curso.codigo_requisitos
+        new_curso.codigo_requisitos = curso_prerequisitos
 
-    print('Ingresa los pre-requisitos, separados con ";", actual: (' +
-          str(curso.codigo_requisitos) + ') ', end='')
-    curso_prerequisitos = parse_requisitos(input()) or curso.codigo_requisitos
-    new_curso.codigo_requisitos = curso_prerequisitos
+        print('Ingresa la opcionalidad, (0: opcional, 1: obligatorio), actual: (' +
+              curso.opcionalidad.name + ') ', end='')
+        curso_opt = parse_opcionalidad(input()) or curso.opcionalidad
+        new_curso.opcionalidad = curso_opt
 
-    print('Ingresa la opcionalidad, (0: opcional, 1: obligatorio), actual: (' +
-          curso.opcionalidad.name + ') ', end='')
-    curso_opt = parse_opcionalidad(input()) or curso.opcionalidad
-    new_curso.opcionalidad = curso_opt
+        print('Ingresa el semestre, actual: (' +
+              str(curso.semestre) + ') ', end='')
+        curso_semestre = int(input() or curso.semestre)
+        new_curso.semestre = curso_semestre
 
-    print('Ingresa el semestre, actual: (' + str(curso.semestre) + ') ', end='')
-    curso_semestre = int(input() or curso.semestre)
-    new_curso.semestre = curso_semestre
+        print('Ingrese los creditos, actual: (' +
+              str(curso.creditos) + ') ', end='')
+        curso_creditos = int(input() or curso.creditos)
+        new_curso.creditos = curso_creditos
 
-    print('Ingrese los creditos, actual: (' + str(curso.creditos) + ') ', end='')
-    curso_creditos = int(input() or curso.creditos)
-    new_curso.creditos = curso_creditos
+        print('Ingrese el estado, (0: aprobado, 1: cursando, -1: pendiente), actual: (' +
+              curso.estado.name + ') ', end='')
+        curso_estado = parse_estado(input()) or curso.estado
+        new_curso.estado = curso_estado
 
-    print('Ingrese el estado, (0: aprobado, 1: cursando, -1: pendiente), actual: (' +
-          curso.estado.name + ') ', end='')
-    curso_estado = parse_estado(input()) or curso.estado
-    new_curso.estado = curso_estado
+        update(database, new_curso, code)
+        pass
 
-    update(database, new_curso, code)
     pass
