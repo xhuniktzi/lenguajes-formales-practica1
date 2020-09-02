c0a857d200585c48a1e5f798164d0bb3234a9dde
Xhunik Miguel <xhuniktzi@gmail.com>
2020-09-02 13:43:43 -0600

    fix: validacion borrar cursos

--- src/controllers/delete_cursos.py
+++ src/controllers/delete_cursos.py
@@ -1,11 +1,17 @@
 # Controlador eliminar cursos
 
 from helpers.database import delete
+from errors.CursoRefError import CursoRefError
 
 
 def exec(database):
     print('*** Eliminar cursos ***')
     print('Ingresa el codigo del curso a eliminar')
     code = int(input())
-    delete(database, code)
+    try:
+        delete(database, code)
+        pass
+    except CursoRefError:
+        print('Este curso es dependencia de otro, no se puede borrar')
+        pass
     pass
