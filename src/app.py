ae5758e774ed9f463c8b7f840db04c2d47385334
Xhunik Miguel <xhuniktzi@gmail.com>
2021-02-15 21:58:00 -0600

    estado temporal inicial, copia de la tarea

---
+++ src/app.py
@@ -0,0 +1,95 @@
+'''
+Programa que debe recibir un archivo como entrada, analizarlo y devolver info.
+por pantalla.
+
+Entrada: Un archivo 'txt' con lineas en este formato
+    [ID]=[Lista de numero separados por comas] [ORDENAR | BUSCAR] [argumentos]
+
+Salida: Por consola en este formato
+    identificador = [ID]
+    Datos = [Array List]
+    funciones = [lista de funciones]
+    salida de la función = [SALIDA BUSCAR | ORDENAR]
+
+Info: Si en una misma linea existen tanto la funcion buscar como ordenar, se
+debe ejecutar la busqueda sobre los datos en bruto (sin ordenar).
+    La funcion de busqueda debe mostrar todas las posiciones donde se encuentra
+una coincidencia, en caso de no existir debe mostrar 'no encontrado'
+'''
+
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+import re as regex
+
+
+def open_file():
+    Tk().withdraw()
+    return askopenfilename()
+
+
+def clear_ops(ops_list: list):
+    ops_return = []
+    op_regex = regex.compile('(BUSCAR|ORDENAR)(\s\d)?')
+    for op in ops_list:
+        if (type(op) is str) and (op != None) and (op != '\n') and (op != '') and (op != ' '):
+            ops_return.append(op_regex.findall(op)[0])
+    return ops_return
+
+
+def execute_op(op: tuple, data: list):
+    if op[0] == 'ORDENAR':
+        data.sort()
+        print('Datos ordenados: {}'.format(data))
+    if op[0] == 'BUSCAR':
+        if int(op[1]) in data:
+            num_matches = data.count(int(op[1]))
+            for i in range(len(data)):
+                if data[i] == int(op[1]):
+                    print('Encontrado {} en posición {}'.format(op[1], i))
+            print('{} encontrado {} veces'.format(int(op[1]), num_matches))
+        else:
+            print('{} no encontrado'.format(int(op[1])))
+
+
+def execute_ops(ops: list, data: list):
+    list_ops = []
+    for op in ops:
+        if op[0] == 'ORDENAR':
+            list_ops.append(op)
+        if op[0] == 'BUSCAR':
+            list_ops.insert(0, op)
+
+    for op in list_ops:
+        execute_op(op, data)
+
+
+def run():
+    file_data = open(open_file(), 'r')
+    file_rows = file_data.readlines()
+
+    for row in file_rows:
+        split_row = row.split('=', 1)
+        print('Identificador: {}'.format(split_row.pop(0)))
+
+        split_op = regex.split('(BUSCAR\s\d$)|(ORDENAR,?)', split_row[0])
+
+        num_regex = regex.compile('\d+,?')
+        clear_numbers = list(map(lambda string: int(
+            string.strip(',')), num_regex.findall(split_op.pop(0))))
+
+        print('Datos: {}'.format(clear_numbers))
+        ops = clear_ops(split_op)
+        execute_ops(ops, clear_numbers)
+        print()
+
+
+def view_menu():
+    print('1. Cargar archivo de entrada')
+    print('2. Desplegar listas ordenadas')
+    print('1. Cargar archivo de entrada')
+    print('1. Cargar archivo de entrada')
+    print('1. Cargar archivo de entrada')
+
+
+if __name__ == '__main__':
+    pass
